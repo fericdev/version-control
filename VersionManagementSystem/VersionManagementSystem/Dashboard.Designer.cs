2ec8551f1a3002bda0f3800d4a1c1bc0ac68ee75
Feric Decenan <feric.decenan.git@gmail.com>
2026-04-04 06:44:13 +0800

    yes

---
+++ VersionManagementSystem/VersionManagementSystem/Dashboard.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace VersionManagementSystem {
+    partial class Dashboard {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            kryptonLinkLabelSelectPath = new Krypton.Toolkit.KryptonLinkLabel();
+            cTextBoxKryptonPath = new FerPROJ.Libraries.UIHelper.WinFormControls.CTextBoxKrypton();
+            cButtonGenerate = new FerPROJ.Libraries.UIHelper.WinFormControls.CButton();
+            ((System.ComponentModel.ISupportInitialize)baseKryptonPanel).BeginInit();
+            baseKryptonPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // baseKryptonPanel
+            // 
+            baseKryptonPanel.Controls.Add(cButtonGenerate);
+            baseKryptonPanel.Controls.Add(cTextBoxKryptonPath);
+            baseKryptonPanel.Controls.Add(kryptonLinkLabelSelectPath);
+            baseKryptonPanel.Size = new Size(623, 178);
+            // 
+            // kryptonLinkLabelSelectPath
+            // 
+            kryptonLinkLabelSelectPath.Location = new Point(34, 52);
+            kryptonLinkLabelSelectPath.Name = "kryptonLinkLabelSelectPath";
+            kryptonLinkLabelSelectPath.Size = new Size(146, 29);
+            kryptonLinkLabelSelectPath.StateCommon.ShortText.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            kryptonLinkLabelSelectPath.TabIndex = 0;
+            kryptonLinkLabelSelectPath.Values.Text = "Select Path:";
+            kryptonLinkLabelSelectPath.LinkClicked += kryptonLinkLabelSelectPath_LinkClicked;
+            // 
+            // cTextBoxKryptonPath
+            // 
+            cTextBoxKryptonPath.Location = new Point(186, 52);
+            cTextBoxKryptonPath.Name = "cTextBoxKryptonPath";
+            cTextBoxKryptonPath.Size = new Size(371, 29);
+            cTextBoxKryptonPath.StateActive.Back.Color1 = Color.WhiteSmoke;
+            cTextBoxKryptonPath.StateActive.Border.Color1 = Color.DarkGray;
+            cTextBoxKryptonPath.StateActive.Border.Color2 = Color.White;
+            cTextBoxKryptonPath.StateActive.Border.Rounding = 10F;
+            cTextBoxKryptonPath.StateActive.Content.Color1 = Color.Black;
+            cTextBoxKryptonPath.StateCommon.Back.Color1 = Color.WhiteSmoke;
+            cTextBoxKryptonPath.StateCommon.Border.Color1 = Color.White;
+            cTextBoxKryptonPath.StateCommon.Border.Color2 = Color.White;
+            cTextBoxKryptonPath.StateCommon.Border.Rounding = 10F;
+            cTextBoxKryptonPath.StateCommon.Content.Color1 = Color.Black;
+            cTextBoxKryptonPath.StateDisabled.Back.Color1 = Color.WhiteSmoke;
+            cTextBoxKryptonPath.StateDisabled.Border.Color1 = Color.White;
+            cTextBoxKryptonPath.StateDisabled.Border.Color2 = Color.White;
+            cTextBoxKryptonPath.StateDisabled.Border.Rounding = 10F;
+            cTextBoxKryptonPath.StateDisabled.Content.Color1 = Color.Black;
+            cTextBoxKryptonPath.StateNormal.Back.Color1 = Color.WhiteSmoke;
+            cTextBoxKryptonPath.StateNormal.Border.Color1 = Color.White;
+            cTextBoxKryptonPath.StateNormal.Border.Color2 = Color.White;
+            cTextBoxKryptonPath.StateNormal.Border.Rounding = 10F;
+            cTextBoxKryptonPath.StateNormal.Content.Color1 = Color.Black;
+            cTextBoxKryptonPath.TabIndex = 1;
+            cTextBoxKryptonPath.Text = "cTextBoxKrypton1";
+            // 
+            // cButtonGenerate
+            // 
+            cButtonGenerate.BackColor = Color.SpringGreen;
+            cButtonGenerate.BackgroundColor = Color.SpringGreen;
+            cButtonGenerate.BorderColor = Color.Green;
+            cButtonGenerate.BorderRadius = 20;
+            cButtonGenerate.BorderSize = 0;
+            cButtonGenerate.FlatAppearance.BorderSize = 0;
+            cButtonGenerate.FlatStyle = FlatStyle.Flat;
+            cButtonGenerate.Font = new Font("Poppins", 9F);
+            cButtonGenerate.ForeColor = Color.Black;
+            cButtonGenerate.Location = new Point(407, 97);
+            cButtonGenerate.Name = "cButtonGenerate";
+            cButtonGenerate.Size = new Size(150, 40);
+            cButtonGenerate.TabIndex = 2;
+            cButtonGenerate.Text = "Generate";
+            cButtonGenerate.TextColor = Color.Black;
+            cButtonGenerate.UseVisualStyleBackColor = false;
+            cButtonGenerate.Click += cButtonGenerate_Click;
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(623, 178);
+            Name = "Dashboard";
+            StateCommon.Back.Color1 = Color.Navy;
+            StateCommon.Back.Color2 = Color.Navy;
+            StateCommon.Border.Color1 = Color.Navy;
+            StateCommon.Border.Color2 = Color.Navy;
+            StateCommon.Header.Back.Color1 = Color.Transparent;
+            StateCommon.Header.Back.Color2 = Color.Transparent;
+            StateCommon.Header.Border.Color1 = Color.Transparent;
+            StateCommon.Header.Border.Color2 = Color.Transparent;
+            Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)baseKryptonPanel).EndInit();
+            baseKryptonPanel.ResumeLayout(false);
+            baseKryptonPanel.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FerPROJ.Libraries.UIHelper.WinFormControls.CButton cButtonGenerate;
+        private FerPROJ.Libraries.UIHelper.WinFormControls.CTextBoxKrypton cTextBoxKryptonPath;
+        private Krypton.Toolkit.KryptonLinkLabel kryptonLinkLabelSelectPath;
+    }
+}
---
+++ VersionManagementSystem/VersionManagementSystem/Dashboard.cs
@@ -0,0 +1,46 @@
+﻿using FerPROJ.Libraries.UIHelper.Files;
+using FerPROJ.Libraries.UIHelper.WinForm.Classes;
+using FerPROJ.Libraries.UIHelper.WinForm.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VersionManagementSystem {
+    public partial class Dashboard : FrmDashboard {
+        public Dashboard() {
+            InitializeComponent();
+        }
+
+        private void kryptonLinkLabelSelectPath_LinkClicked(object sender, EventArgs e) {
+            using (OpenFileDialog ofd = new OpenFileDialog()) {
+                ofd.Filter = "Executable Files (*.exe)|*.exe|All Files (*.*)|*.*";
+
+                if (ofd.ShowDialog() == DialogResult.OK) {
+                    cTextBoxKryptonPath.Text = ofd.FileName;
+                }
+            }
+        }
+
+        private async void cButtonGenerate_Click(object sender, EventArgs e) {
+            var fileInfo = new FileInfo(cTextBoxKryptonPath.Text);
+            var versionInfo = FileVersionInfo.GetVersionInfo(cTextBoxKryptonPath.Text);
+            var versionModel = new VersionModel {
+                SystemName = Path.GetFileNameWithoutExtension(fileInfo.Name).Replace(".", ""),
+                SystemVersion = versionInfo.FileVersion,
+                DateUpdated = fileInfo.LastWriteTime.ToShortDateString(),
+            };
+            await versionModel.GetOrCreateDataAsync<VersionModel>($"{versionModel.SystemName}_version", false, false, versionModel.SystemName);
+            DialogManager.Info("Version information generated and saved successfully.", "Success");
+        }
+    }
+    public class VersionModel {
+        public string SystemName { get; set; }
+        public string SystemVersion { get; set; }
+        public string DateUpdated { get; set; }
+    }
+}
